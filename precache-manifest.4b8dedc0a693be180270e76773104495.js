f4e91b9a052dc7521b7d9e945ef67ccf3e1d495c
Omar Faruque <webdevwithomar@gmail.com>
2020-02-01 16:21:29 +0600

    Updates

---
+++ precache-manifest.4b8dedc0a693be180270e76773104495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421dbb72180d607baa2353c7f1d05247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "269e41640a299ad534a1",
+    "url": "/static/css/main.5fb2eac2.chunk.css"
+  },
+  {
+    "revision": "b3b556a8cdd80a391700",
+    "url": "/static/js/2.47aca6db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.47aca6db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "269e41640a299ad534a1",
+    "url": "/static/js/main.ee771fb1.chunk.js"
+  },
+  {
+    "revision": "3b610417cf7ee912ed69",
+    "url": "/static/js/runtime-main.6b16114b.js"
+  }
+]);
